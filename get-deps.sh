bd68ed37fefbf4e945b4d3c77c1c9adee0b7117c
Rod Smith <work.rod.smith@gmail.com>
2021-03-31 09:03:14 +0100

    tidy up

--- get-deps.sh
+++ get-deps.sh
@@ -9,8 +9,6 @@ if ! which govendor >/dev/null;then
 
     if ! which govendor >/dev/null;then
 	    echo Installing govendor
-        echo "current location: $(pwd)"
-        echo "GOPATH: $GOPATH"
 	    go get -u github.com/kardianos/govendor
     fi
 fi
